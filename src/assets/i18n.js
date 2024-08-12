e1ad8fd2d31ccb0182ea92a2f6a0a1cbe8c709fd
Amanda <amandard94@gmail.com>
2024-08-12 11:57:55 -0300

    feat: about about revisioned

--- src/assets/i18n.js
+++ src/assets/i18n.js
@@ -38,7 +38,18 @@ const resources = {
       "The Controller of personal data is Swell IT Solutions. The data entered in the form will be processed solely and exclusively to maintain contact.": "The Controller of personal data is Swell IT Solutions. The data entered in the form will be processed solely and exclusively to maintain contact.",
       "You can withdraw any consent given at any time. For additional information or to exercise your rights, please visit the": "You can withdraw any consent given at any time. For additional information or to exercise your rights, please visit the",
       "privacy notice": "privacy notice",
-      "Swell does not use contact information to send any type of SPAM.": "Swell does not use contact information to send any type of SPAM."
+      "Swell does not use contact information to send any type of SPAM.": "Swell does not use contact information to send any type of SPAM.",
+      "about_section1_title": "About Swell",
+      "about_section1": "We are <span className='blue-text'>Swell It Solutions</span>. More than just a Technology and Management consultancy, we are a community of diverse talents. United by a passion for overcoming complex challenges, our DNA is an unparalleled blend of innovation, expertise, and commitment.",
+      "about_section2_title": "Our 20+ Years of Experience",
+      "about_section2": "With a solid history of over two decades in the market, <span className='blue-text'>Swell It Solutions</span> has amassed extensive experience in Software Development and Management. Our journey is marked by successful partnerships across a wide range of sectors, from agile and innovative Startups to large Enterprises.",
+      "about_section3_title": "The Journey of Innovation",
+      "about_section3": "We are pioneers in applying the latest technology trends and best management practices, tailoring them to the specific needs of our clients.",
+      "about_section4_title": "Our Holistic Approach",
+      "about_section4": "Our holistic approach sets us apart as we do not just deliver solutions but also create lasting partnerships based on mutual trust. We work closely with our clients, deeply understanding their objectives and challenges to offer tailored solutions that drive long-term success.",
+      "about_section5_title": "A World of Possibilities",
+      "about_section5": "At <span className='blue-text'>Swell It Solutions</span>, we believe that no challenge is too great or problem too complex. Every challenge is an opportunity for innovation, every problem a doorway to creative solutions. We build our journey of constant evolution, where the future is shaped by our passion for technology and our commitment to your success.",
+      "about_section5.1": "Together, we are ready to explore an infinite world of possibilities."
     }
   },
   pt: {
@@ -77,7 +88,18 @@ const resources = {
       "The Controller of personal data is Swell IT Solutions. The data entered in the form will be processed solely and exclusively to maintain contact.": "O Controlador dos dados pessoais é a Swell IT Solutions. Os dados inseridos no formulário serão processados única e exclusivamente para manter contato.",
       "You can withdraw any consent given at any time. For additional information or to exercise your rights, please visit the": "Você pode retirar qualquer o consentimento dado a qualquer momento. Para obter informações adicionais ou para exercer os seus direitos, visite o",
       "privacy notice": "aviso de privacidade",
-      "Swell does not use contact information to send any type of SPAM.": "A Swell não utiliza informações de contato para enviar qualquer tipo de SPAM."
+      "Swell does not use contact information to send any type of SPAM.": "A Swell não utiliza informações de contato para enviar qualquer tipo de SPAM.",
+      "about_section1_title": "A Swell",
+      "about_section1": "Somos a <span className='blue-text'>Swell It Solutions</span>. Mais do que uma consultoria em Tecnologia e Gestão, uma comunidade de talentos diversos. Unidos pela paixão em superar desafios complexos, nosso DNA é uma mistura inigualável de inovação, expertise e comprometimento.",
+      "about_section2_title": "Nossa Experiência de Mais de 20 Anos",
+      "about_section2": "Com uma sólida história de mais de duas décadas no mercado, <span className='blue-text'>a Swell It Solutions</span> acumulou uma vasta experiência em Desenvolvimento e Gestão de Software. Nossa jornada é marcada por parcerias de sucesso com uma ampla gama de setores, desde Startups ágeis e inovadoras até grandes Empresas.",
+      "about_section3_title": "A Jornada da Inovação",
+      "about_section3": "Somos pioneiros na aplicação das últimas tendências em tecnologia e melhores práticas de gestão, adaptando-as às necessidades específicas de nossos clientes.",
+      "about_section4_title": "Nossa Abordagem Holística",
+      "about_section4": "Nossa abordagem holística nos diferencia pois não apenas entregamos soluções, mas também criamos parcerias duradouras baseadas na confiança mútua. Colaboramos de perto com nossos clientes, entendendo profundamente seus objetivos e desafios para oferecer soluções sob medida que impulsionam o sucesso a longo prazo.",
+      "about_section5_title": "Um Mundo de Possibilidades",
+      "about_section5": "Na <span className='blue-text'>Swell It Solutions</span>, acreditamos que não há desafio muito grande nem problema muito complexo. Cada desafio é uma oportunidade de inovação, cada problema uma porta para soluções criativas. Construímos nossa jornada de constante evolução, onde o futuro é moldado por nossa paixão pela tecnologia e nossa dedicação ao seu sucesso.",
+      "about_section5.1": "Juntos, estamos prontos para explorar um mundo infinito de possibilidades."
     }
   },
   es: {
@@ -115,8 +137,18 @@ const resources = {
       "The Controller of personal data is Swell IT Solutions. The data entered in the form will be processed solely and exclusively to maintain contact.": "El Responsable del tratamiento de los datos personales es Swell IT Solutions. Los datos introducidos en el formulario serán tratados única y exclusivamente para mantener el contacto.",
       "You can withdraw any consent given at any time. For additional information or to exercise your rights, please visit the": "Puede retirar cualquier consentimiento prestado en cualquier momento. Para obtener información adicional o ejercer sus derechos, visite el",
       "privacy notice": "aviso de privacidad",
-      "Swell does not use contact information to send any type of SPAM.": "Swell no utiliza la información de contacto para enviar ningún tipo de SPAM."
-
+      "Swell does not use contact information to send any type of SPAM.": "Swell no utiliza la información de contacto para enviar ningún tipo de SPAM.",
+      "about_section1_title": "Acerca de Swell",
+      "about_section1": "Somos <span className='blue-text'>Swell It Solutions</span>. Más que una consultoría en Tecnología y Gestión, somos una comunidad de talentos diversos. Unidos por la pasión de superar desafíos complejos, nuestro ADN es una mezcla inigualable de innovación, experiencia y compromiso.",
+      "about_section2_title": "Nuestra Experiencia de Más de 20 Años",
+      "about_section2": "Con una sólida historia de más de dos décadas en el mercado, <span className='blue-text'>Swell It Solutions</span> ha acumulado una vasta experiencia en Desarrollo y Gestión de Software. Nuestra trayectoria está marcada por asociaciones exitosas con una amplia gama de sectores, desde Startups ágiles e innovadoras hasta grandes Empresas.",
+      "about_section3_title": "El Viaje de la Innovación",
+      "about_section3": "Somos pioneros en la aplicación de las últimas tendencias en tecnología y mejores prácticas de gestión, adaptándolas a las necesidades específicas de nuestros clientes.",
+      "about_section4_title": "Nuestro Enfoque Holístico",
+      "about_section4": "Nuestro enfoque holístico nos diferencia, ya que no solo entregamos soluciones, sino que también creamos asociaciones duraderas basadas en la confianza mutua. Colaboramos estrechamente con nuestros clientes, entendiendo profundamente sus objetivos y desafíos para ofrecer soluciones a medida que impulsan el éxito a largo plazo.",
+      "about_section5_title": "Un Mundo de Posibilidades",
+      "about_section5": "En <span className='blue-text'>Swell It Solutions</span>, creemos que no hay desafío demasiado grande ni problema demasiado complejo. Cada desafío es una oportunidad de innovación, cada problema una puerta a soluciones creativas. Construimos nuestra trayectoria de constante evolución, donde el futuro está moldeado por nuestra pasión por la tecnología y nuestro compromiso con su éxito.",
+      "about_section5.1": "Juntos, estamos listos para explorar un mundo infinito de posibilidades."
     }
   },
   jp: {
@@ -154,8 +186,18 @@ const resources = {
       "The Controller of personal data is Swell IT Solutions. The data entered in the form will be processed solely and exclusively to maintain contact.": "個人データの管理者は Swell IT Solutions です。フォームに入力されたデータは、連絡を維持するためにのみ処理されます。",
       "You can withdraw any consent given at any time. For additional information or to exercise your rights, please visit the": "与えられた同意はいつでも撤回できます。追加情報または権利を行使するには、プライバシー通知をご覧ください。",
       "privacy notice": "プライバシー通知",
-      "Swell does not use contact information to send any type of SPAM.": "Swell は、連絡先情報を使用してスパムメールを送信することはありません。"
-
+      "Swell does not use contact information to send any type of SPAM.": "Swell は、連絡先情報を使用してスパムメールを送信することはありません。",
+      "about_section1_title": "スウェル",
+      "about_section1": "私たちは<span className='blue-text'>Swell It Solutions</span>です。 テクノロジーとマネジメントのコンサルティングを超えた、多様な才能のコミュニティです。 複雑な課題を克服する情熱によって団結し、私たちのDNAはイノベーション、専門知識、そしてコミットメントの比類なき融合です。",
+      "about_section2_title": "20年以上の経験",
+      "about_section2": "市場での20年以上の確固たる歴史を持つ、<span className='blue-text'>Swell It Solutions</span>は、ソフトウェアの開発と管理において豊富な経験を蓄積してきました。 私たちの旅は、アジャイルで革新的なスタートアップから大企業に至るまで、幅広い業界での成功したパートナーシップによって特徴付けられています。",
+      "about_section3_title": "イノベーションの旅",
+      "about_section3": "私たちは、最新の技術トレンドとベストプラクティスを適用し、それをクライアントの特定のニーズに合わせて調整する先駆者です。",
+      "about_section4_title": "包括的なアプローチ",
+      "about_section4": "私たちのホリスティックなアプローチは、単にソリューションを提供するだけでなく、相互の信頼に基づいた長期的なパートナーシップを築くことです。 クライアントと密に連携し、彼らの目標と課題を深く理解し、成功を長期的に促進するためのカスタマイズされたソリューションを提供します。",
+      "about_section5_title": "無限の可能性",
+      "about_section5": "<span className='blue-text'>Swell It Solutions</span>では、あらゆる課題があまりにも大きすぎたり、問題が複雑すぎたりすることはないと考えています。 各課題はイノベーションの機会であり、各問題は創造的なソリューションへの扉です。 テクノロジーへの情熱とあなたの成功への献身によって形作られる、常に進化する私たちの旅を築いています。",
+      "about_section5.1": "共に、私たちは無限の可能性の世界を探求する準備ができています。"
     }
   },
   // Add other languages here
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import BoasVindasVideo from '../../assets/BoasVindasVideo.mp4'
 import SwellFlux from '../../assets/images/nossa-historia-pt.gif'
 import './style.css';
 
 const About = () => {
+  const { t } = useTranslation();
+
   return (
     <>
         <video className="video-fill webkit-fill" autoPlay muted loop>
@@ -13,47 +16,48 @@ const About = () => {
 
         <div className="container small-container gap-0 pt-5 pt-5 text-align-center">
             "          
-                <p>Com mais de 20 anos de experiência, cultivamos uma comunidade de talentos diversos, impulsionada pela inovação e dedicada a fornecer soluções personalizadas. Nossa jornada tecnológica é marcada pela contínua evolução, onde cada avanço nos leva mais perto do amanhã.</p>
+                <p>{t('swell_description')}</p>
             "
         </div>
 
-        <div className="section-wrapper section-width">
-            <img src={SwellFlux} className="container small-container gap-0" alt="swell-flux" />
+        <div className="section-wrapper section-width pd-0">
+            <img src={SwellFlux} className="container img-fluid gap-0" alt="swell-flux" />
         </div>
 
         <div className='container small-container pt-7 gap-0'>
-            <h2 className='mb-5'>A Swell</h2>
+            <h2 className='mb-5'>{t('about_section1_title')}</h2>
 
-            <p>
-                Somos a <span className='blue-text'>Swell It Solutions</span> Mais do que uma consultoria em Tecnologia e Gestão, uma comunidade de talentos diversos. Unidos pela paixão em superar desafios complexos, nosso DNA é uma mistura inigualável de inovação, expertise e comprometimento.
-            </p>
+            <p dangerouslySetInnerHTML={{ __html: t('about_section1') }} />
 
             <div>
-                <h2 className='dark-title'>Nossa Experiência de Mais de 20 Anos</h2>
+                <h2 className='dark-title'>{t('about_section2_title')}</h2>
+
+                <p dangerouslySetInnerHTML={{ __html: t('about_section2') }} />
+
+            </div>
+
+            <div>
+                <h2 className='dark-title'>{t('about_section3_title')}</h2>
                 <p>
-                    Com uma sólida história de mais de duas décadas no mercado, <span className='blue-text'>a Swell It Solutions</span> acumulou uma vasta experiência em Desenvolvimento e Gestão de Software. Nossa jornada é marcada por parcerias de sucesso com uma ampla gama de setores, desde Startups ágeis e inovadoras até grandes Empresas.
+                {t('about_section3')}
+                </p>
+            </div>
+
+
+            <div>
+                <h2 className='dark-title'>{t('about_section4_title')}</h2>
+                <p>
+                {t('about_section4')}
                 </p>
             </div>
 
             <div>
-                <h2 className='dark-title'>A Jornada da Inovação</h2>
-                <p>
-                    Somos pioneiros na aplicação das últimas tendências em tecnologia e melhores práticas de gestão, adaptando-as às necessidades específicas de nossos clientes.
-                </p>
-            </div>
+                <h2 className='dark-title'>{t('about_section5_title')}</h2>
+                    
+                <p className='mb-0' dangerouslySetInnerHTML={{ __html: t('about_section5') }} />
 
+                <p dangerouslySetInnerHTML={{ __html: t('about_section5.1') }} />
 
-            <div>
-                <h2 className='dark-title'>Nossa Abordagem Holística</h2>
-                <p>
-                    Nossa abordagem holística nos diferencia pois não apenas entregamos soluções, mas também criamos parcerias duradouras baseadas na confiança mútua. Colaboramos de perto com nossos clientes, entendendo profundamente seus objetivos e desafios para oferecer soluções sob medida que impulsionam o sucesso a longo prazo.
-                </p>
-            </div>
-
-            <div>
-                <h2 className='dark-title'>Um Mundo de Possibilidades</h2>
-                <p className='mb-0'>Na <span className='blue-text'>Swell It Solutions</span>, acreditamos que não há desafio muito grande nem problema muito complexo. Cada desafio é uma oportunidade de inovação, cada problema uma porta para soluções criativas. Construímos nossa jornada de constante evolução, onde o futuro é moldado por nossa paixão pela tecnologia e nossa dedicação ao seu sucesso. </p>
-                <p>Juntos, estamos prontos para explorar um mundo infinito de possibilidades.</p>  
             </div>
 
             <div className='center-div pt-5 pb-5'>
