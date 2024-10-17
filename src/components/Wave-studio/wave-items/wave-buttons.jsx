fbed301ab07e516f9e4a772e9d9023ff69ae7735
Amanda <amandard94@gmail.com>
2024-10-17 00:17:40 -0300

    feat: path de botoes wave studio

--- src/components/Wave-studio/wave-items/wave-buttons.jsx
+++ src/components/Wave-studio/wave-items/wave-buttons.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import TechSVG from './TechSvg'
+import BusinessSvg from './BusinessSvg'
+import TalentSvg from './TalentSvg';
+import DiscoverySvg from './DiscoverySvg';
+import WaveTitle from '../../../assets/images/wave-studio/WaveTechTitle.png'
 
 const WaveButton = ({ setActiveComponent }) => {
   const handleScrollAndSetActive = (component) => {
@@ -11,11 +16,34 @@ const WaveButton = ({ setActiveComponent }) => {
   };
 
   return (
-    <div className="flex gap-4">
-      <button onClick={() => handleScrollAndSetActive('discovery')}>Wave Discovery</button>
-      <button onClick={() => handleScrollAndSetActive('tech')}>Wave Tech</button>
-      <button onClick={() => handleScrollAndSetActive('business')}>Wave Business</button>
-      <button onClick={() => handleScrollAndSetActive('talent')}>Wave Talent</button>
+    <div className="relative w-full h-[500px]">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <img src={WaveTitle} alt="Wave Title" />
+        </div>
+
+        <div className="absolute top-[-20%] left-1/2 transform -translate-x-1/2"
+          onClick={() => handleScrollAndSetActive('discovery')} style={{ cursor: 'pointer' }}
+        >
+            <DiscoverySvg />
+        </div>
+
+        <div className="absolute left-[-4%] top-1/2 transform -translate-y-1/2"
+          onClick={() => handleScrollAndSetActive('tech')} style={{ cursor: 'pointer' }}
+        >
+            <TechSVG />
+        </div>
+
+        <div className="absolute right-[-4%] top-1/2 transform -translate-y-1/2"
+          onClick={() => handleScrollAndSetActive('business')} style={{ cursor: 'pointer' }}
+        >
+            <BusinessSvg />
+        </div>
+
+        <div className="absolute bottom-[-20%] left-1/2 transform -translate-x-1/2"
+          onClick={() => handleScrollAndSetActive('talent')} style={{ cursor: 'pointer' }}
+        >
+            <TalentSvg />
+        </div>
     </div>
   );
 };
